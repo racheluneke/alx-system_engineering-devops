2b888df02c64c02fa0fc5f0839b0d5c452a18dfd
racheluneke <rackelchi07@gmail.com>
2023-10-03 12:25:19 -0700

    regex

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-#regex expression matction two patterns
-
-puts ARGV[0].scan(/hb?tn/).join
--- 0x06-regular_expressions/README.md
+++ 0x06-regular_expressions/README.md
@@ -1,1 +1,1 @@
-regular_expressions  are simple but know they are all characters
+my first readme on regex
